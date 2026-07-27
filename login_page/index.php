590041849dfc1f13f957c6f1540a833af24d4bf0
Purushottam Chandrakar <78301297+pankaj135@users.noreply.github.com>
2026-07-28 01:08:56 +0530

    Add files via upload

---
+++ login_page/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <nav>
+            <center>
+                <h1>Welcome to the Login Page</h1>
+            </center>
+        </nav>
+        <div class="container">
+                <form action="">
+
+                    <table>
+                        <tr>
+                            <th>
+                              <font>
+                                Login   
+                              </font>
+
+                            </th>
+                            
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" placeholder="Username" name="username">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="password" placeholder="Password" name="password">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="text-align: center; " class="submit_row">
+                                <input type="submit" value="Login">
+                            </td>
+                    </table>
+                </form>
+        </div>
+    </body>
+</html>
